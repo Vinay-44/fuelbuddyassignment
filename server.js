e374e0ba502528db2179f992f4e95ebf687fd409
Vinay-44 <vinaymudgal441163@gmail.com>
2025-02-28 04:47:11 +0530

    added swagger

--- server.js
+++ server.js
@@ -8,6 +8,24 @@ const fastify = Fastify({
 });
 
 
+await fastify.register(import('@fastify/swagger'))
+
+await fastify.register(import('@fastify/swagger-ui'), {
+  routePrefix: '/docs',
+  uiConfig: {
+    docExpansion: 'full',
+    deepLinking: false
+  },
+  uiHooks: {
+    onRequest: function (request, reply, next) { next() },
+    preHandler: function (request, reply, next) { next() }
+  },
+  staticCSP: true,
+  transformStaticCSP: (header) => header,
+  transformSpecification: (swaggerObject, request, reply) => { return swaggerObject },
+  transformSpecificationClone: true
+})
+
 fastify.register(user,{prefix:'/user'});
 
 fastify.listen({port:3000}).then((val)=>{
